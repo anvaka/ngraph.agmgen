2b653781fbd147dd2919805b8a8355c2d7de50e3
anvaka <anvaka@gmail.com>
2015-03-30 22:24:14 -0700

    Added documentation

--- README.md
+++ README.md
@@ -1,8 +1,82 @@
 # ngraph.agmgen [![Build Status](https://travis-ci.org/anvaka/ngraph.agmgen.svg)](https://travis-ci.org/anvaka/ngraph.agmgen)
 
-Graph generator based on affiliation graph model (AGM). Very early alpha.
+Based on affiliation graph constructs a social graph.
 
-See http://cs.stanford.edu/people/jure/pubs/bigclam-wsdm13.pdf for more details.
+See also [Overlapping Community Detection at Scale](http://cs.stanford.edu/people/jure/pubs/bigclam-wsdm13.pdf)
+for more details.
+
+# what?
+
+`AGM` is a very interesting graph generator model, which can generate many different
+kinds of graphs.
+
+AGM uses `affiliation graph` as its input and produces a "social" graph as its
+output. `Affiliation graph` consists of two kinds of nodes:
+
+* Community node - this node represents a community (e.g. a mailing list, a news group, etc.)
+* Member node - a person who is part of a community.
+
+Each edge in this graph points from a member node to a community node. If there
+is an edge between node `A` and `B` it means that person `A` belongs to community `B`.
+
+A social graph is just a regular graph, which includes all member nodes from
+affiliation graph. Each edge between nodes in social graph is created at
+random and is driven by number of shared communities. The more shared communities
+two nodes have, the more likely they will receive an edge in "social" graph.
+
+# usage
+
+``` js
+// First we need to construct affiliation graph. Let's assume this is
+// our graph:
+var affiliationGraph = require('ngraph.fromdot')([
+  // first group:
+  "'user 0' -> 'group 1'",
+  "'user 1' -> 'group 1'",
+  "'user 2' -> 'group 1'",
+
+  // second group:
+  "'user 1' -> 'group 2'",
+  "'user 2' -> 'group 2'",
+  "'user 3' -> 'group 2'"
+].join('\n'));
+
+// now let's generate social graph:
+var agm = require('ngraph.agmgen');
+var graph = agm(affiliationGraph);
+```
+
+The final `graph` instance will include all users as members, and some links.
+Since links are determined at random, different seeds can have different results.
+
+In this particular case the default seed `42` was used and resulted in the following
+social graph:
+
+```
+digraph G {
+  "user 0" -> "user 1"
+  "user 0" -> "user 2"
+  "user 2" -> "user 3"
+}
+```
+
+If you want to pass different seed you can use optional second argument:
+
+``` js
+var seed = 1;
+var graph = agm(affiliationGraph, { random: seed });
+```
+
+Now results will be:
+
+```
+digraph G {
+  "user 0" -> "user 1"
+  "user 1" -> "user 2"
+  "user 2" -> "user 0"
+  "user 2" -> "user 3"
+}
+```
 
 install
 =======
--- index.js
+++ index.js
@@ -1,14 +1,54 @@
+/**
+ * Based on affiliation graph constructs a social graph. Affiliation graph
+ * consists of two kinds of nodes:
+ *  - Community node - this could be a mailing list, a news group, etc.
+ *  - Member node - a person who is part of a community.
+ *
+ * Each edge in this graph is assumed to point from a member node to a community
+ * node. If there is an edge between node `A` and `B` it means that person `A`
+ * belongs to community `B`.
+ *
+ * A social graph is just a regular graph, which includes all member nodes from
+ * affiliation graph. Each edge between nodes in social graph is created at
+ * random and is driven by number of shared communities.
+ */
 var createGraph = require('ngraph.graph');
 var randomAPI = require('ngraph.random');
 
 module.exports = agm;
 
-function agm(affiliationGraph, random) {
-  var graph = createGraph();
-  random = random || randomAPI.random(42);
+/**
+ * @param {ngraph.graph} affiliationGraph - initial model.
+ * @param {Object+} options - configuration of the algorithm, with possible keys:
+ *  options.random - seeded random number generator, `ngraph.random` is used by default
+ *  options.coefficient - power law coefficient for weights distribution. 0.6 by default
+ *  options.scale - power law scale coefficient for weights distribution. 1.3 by default
+ */
+function agm(affiliationGraph, options) {
+  /**
+   * In nutshell our code here implements the following algorithm:
+   * 1. Add all member nodes from affiliation graph into social graph.
+   * 2. For each member of the final graph build edges. Probability of a node
+   * `u` forming edge with node `v` is calculated by:
+   *   p(u, v) = 1 - Math.pow(Math.E, -weight);
+   * where `weight` is calculated based on total number of shared communities.
+   */
+  options = options || {};
+  var graph = createGraph(); // The final graph that we are trying to construct.
+  var random = randomAPI.random(typeof options.random === 'number' ? options.random : 42);
 
-  ensureAffiliationWeightsInitialized(affiliationGraph);
+  // Each edge in the affiliation graph needs to have a "weight". It denotes
+  // how strongly a given member belongs to a community. The higher its weight
+  // the more probable its connection with other members.
+  //
+  // If client has passed weights, we will use them; Otherwise we would just
+  // assume power law weights distribution inside a community
+  ensureAffiliationWeightsInitialized(affiliationGraph, options);
+
+  // Now let's add all members into final graph:
   affiliationGraph.forEachNode(addMemberNodes);
+
+  // The easy step is done, let's try to build edges, following our model:
   graph.forEachNode(addEdges);
 
   return graph;
@@ -22,26 +62,25 @@ function agm(affiliationGraph, random) {
   }
 
   function addEdges(source) {
+    // For each other member with whom `source` member shares communities, lets
+    // compute their total weight (Fu * Fv)
+    // neighborsWeight is a hash map, where key is other member id, and value
+    // is total weight.
     var neighborsWeight = Object.create(null);
+    // we will be processing all communities of the `source` member, and to
+    // simplify computation of the `Fu * Fv` we will remember weight of the
+    // source node in the community that is currently being processed:
     var sourceWeightInCurrentCommunity;
 
+    // now we visit each community one by one to compute Fu * Fv:
     affiliationGraph.forEachLinkedNode(source.id, processCommunity, true);
+
+    // The neighborsWeight is ready, we can visit every neighbor and consider
+    // linking it with the current `source` node
     addEdgesMaybe();
 
+    // At this moment we are done processing `source` member.
     return;
-
-    function addEdgesMaybe() {
-      var neighbors = Object.keys(neighborsWeight);
-
-      for (var i = 0; i < neighbors.length; ++i) {
-        var neighborId = neighbors[i];
-        var weight = neighborsWeight[neighborId];
-        var probability = 1 - Math.pow(Math.E, -weight);
-        if (random.nextDouble() <= probability) {
-          graph.addLink(source.id, neighborId);
-        }
-      }
-    }
 
     function processCommunity(communityNode, link) {
       sourceWeightInCurrentCommunity = link.data;
@@ -53,17 +92,34 @@ function agm(affiliationGraph, random) {
       if (otherId === source.id) {
         return; // same node
       }
-      // todo: this is painfully slow, update ngraph.graph to be faster.
+
+      // todo: this is painfully slow, since ngraph.graph performs linear search
+      // on `hasLink`. Update ngraph.graph to be faster.
       if (graph.hasLink(source.id, otherId) ||
           graph.hasLink(otherId, source.id)) {
         return; // edge already there
       }
-      // Accumulate probability of an edge [source -> other]
+
+      // Accumulate probability of an edge [source -> other]. Remember,
+      // sourceWeightInCurrentCommunity every time when we visit new community:
       var communityLinkWeight = memberCommunityEdge.data * sourceWeightInCurrentCommunity;
       if (neighborsWeight[otherId]) {
         neighborsWeight[otherId] += communityLinkWeight;
       } else {
         neighborsWeight[otherId] = communityLinkWeight;
+      }
+    }
+
+    function addEdgesMaybe() {
+      var neighbors = Object.keys(neighborsWeight);
+
+      for (var i = 0; i < neighbors.length; ++i) {
+        var neighborId = neighbors[i];
+        var weight = neighborsWeight[neighborId];
+        var probability = 1 - Math.pow(Math.E, -weight);
+        if (random.nextDouble() <= probability) {
+          graph.addLink(source.id, neighborId);
+        }
       }
     }
   }
@@ -78,7 +134,10 @@ function agm(affiliationGraph, random) {
   }
 }
 
-function ensureAffiliationWeightsInitialized(graph) {
+function ensureAffiliationWeightsInitialized(graph, options) {
+  var scale = typeof options.scale === 'number' ? options.scale : 1.3;
+  var coefficient = typeof options.coefficient === 'number' ? options.coefficient : 0.6;
+
   graph.forEachLink(ensureLinkInitialized);
 
   function ensureLinkInitialized(link) {
@@ -86,7 +145,6 @@ function ensureAffiliationWeightsInitialized(graph) {
 
     var communityNodeId = link.toId;
     var membersCount = graph.getLinks(communityNodeId).length;
-    // todo: let users configure scale and the coefficient
-    link.data = Math.pow(membersCount, -0.6) * 1.3;
+    link.data = Math.pow(membersCount, -coefficient) * scale;
   }
 }
